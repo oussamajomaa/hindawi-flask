038aac20cf6a69fec0e4483daa7dc71140a435e9
oussamajomaa <osm70@gmx.com>
2022-11-30 08:18:45 +0100

    path

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import request as req
 from flask_cors import CORS
 from bs4 import BeautifulSoup
 import json
+import os
 
 
 logging.basicConfig(level=logging.INFO)
@@ -45,7 +46,7 @@ def get_books():
     return all_books
 
 from pathlib import Path
-downloads_path = str(Path.home() / "Downloads")
+downloads_path = str(os.path.join(Path.home(), "Downloads"))
 
 @app.route('/downlaod')
 def downlaod_file():
@@ -63,4 +64,24 @@ def downlaod_file():
 
 if __name__ == '__main__':
     app.run()
+
+
+# beautifulsoup4==4.11.1
+# bs4==0.0.1
+# certifi==2022.9.24
+# charset-normalizer==2.1.1
+# click==8.1.3
+# Flask==2.2.2
+# Flask-Cors==3.0.10
+# idna==3.4
+# itsdangerous==2.1.2
+# Jinja2==3.1.2
+# MarkupSafe==2.1.1
+# pathlib==1.0.1
+# requests==2.28.1
+# six==1.16.0
+# soupsieve==2.3.2.post1
+# urllib3==1.26.13
+# Werkzeug==2.2.2
+# gunicorn==20.1.0
 
